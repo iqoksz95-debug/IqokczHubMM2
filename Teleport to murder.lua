41470b51afc083666f6a943a5e6b253d82158a8e
iqoksz95-debug <artyplayzyt@gmail.com>
2025-03-19 19:09:57 +0200

    Update Teleport to murder.lua

--- Teleport to murder.lua
+++ Teleport to murder.lua
@@ -1,3 +1,16 @@
+-- Получение локального игрока
+local Plr = game:GetService("Players").LocalPlayer
+
+-- Функция для поиска убийцы
+function GetMurderer()
+    for i, v in pairs(game:GetService("Players"):GetPlayers()) do
+        if (v.Backpack:FindFirstChild("Knife") or (v.Character and v.Character:FindFirstChild("Knife"))) then
+            return v.Character
+        end
+    end
+    return nil
+end
+
 -- Функция для телепортации к убийце
 local function TeleportToMurderer()
     Plr.Character.HumanoidRootPart.CFrame = GetMurderer() ~= nil and GetMurderer().HumanoidRootPart.CFrame or Plr.Character.HumanoidRootPart.CFrame
