b519a4fa6547c5e65b8098c55d2d360c758dd6ae
iqoksz95-debug <artyplayzyt@gmail.com>
2025-03-19 20:16:06 +0200

    Update AutoShootMurderer.lua

--- AutoShootMurderer.lua
+++ AutoShootMurderer.lua
@@ -1,49 +1,66 @@
 -- Получение локального игрока
 local Plr = game:GetService("Players").LocalPlayer
 
--- Функция для поиска GunDrop в workspace (включая вложенные части)
-local function FindGunDrop()
-    -- Рекурсивно ищем GunDrop в workspace
-    local function searchInModel(model)
-        for _, child in pairs(model:GetChildren()) do
-            if child.Name == "GunDrop" then
-                return child
-            elseif child:IsA("Model") or child:IsA("Folder") then
-                local found = searchInModel(child)
-                if found then
-                    return found
-                end
-            end
+-- Функция для поиска убийцы
+function GetMurderer()
+    for _, v in pairs(game:GetService("Players"):GetPlayers()) do
+        if (v.Backpack:FindFirstChild("Knife") or v.Character and v.Character:FindFirstChild("Knife")) and v ~= Plr then
+            return v.Character
         end
-        return nil
     end
-
-    -- Начинаем поиск с workspace
-    return searchInModel(workspace)
+    return nil
 end
 
--- Функция для телепортации к пистолету
-local function TeleportToGun()
-    -- Ищем объект GunDrop
-    local gunDrop = FindGunDrop()
-    
-    -- Проверяем, существует ли GunDrop
-    if gunDrop then
-        -- Проверяем, есть ли у игрока персонаж и HumanoidRootPart
-        if Plr.Character and Plr.Character:FindFirstChild("HumanoidRootPart") then
-            -- Телепортируем игрока к пистолету с небольшим смещением по оси Y
-            Plr.Character.HumanoidRootPart.CFrame = gunDrop.CFrame + Vector3.new(0, 2, 0)
-            print("Телепортация к пистолету выполнена!")
+-- Функция для автоматической стрельбы по убийце
+function AutoShootMurderer()
+    -- Проверяем, есть ли у игрока пистолет
+    if Plr.Character and (Plr.Character:FindFirstChild("Gun") or Plr.Backpack:FindFirstChild("Gun")) then
+        -- Перемещаем пистолет в руку, если он в рюкзаке
+        if Plr.Backpack:FindFirstChild("Gun") then
+            Plr.Backpack.Gun.Parent = Plr.Character
+        end
+
+        -- Получаем убийцу
+        local Murderer = GetMurderer()
+        if Murderer then
+            -- Получаем пистолет
+            local Gun = Plr.Character:FindFirstChild("Gun")
+            if Gun then
+                -- Цикл стрельбы по убийце
+                while Murderer and Murderer:FindFirstChild("Humanoid") and Murderer.Humanoid.Health > 0 do
+                    -- Стреляем в убийцу
+                    local args = {
+                        [1] = 1, -- Количество выстрелов
+                        [2] = Murderer.HumanoidRootPart.Position, -- Позиция убийцы
+                        [3] = "AH" -- Тип выстрела (можно изменить в зависимости от игры)
+                    }
+                    Gun.KnifeServer.ShootGun:InvokeServer(unpack(args))
+                    
+                    -- Ждем перед следующим выстрелом
+                    task.wait(0.5)
+                    
+                    -- Обновляем информацию об убийце
+                    Murderer = GetMurderer()
+                end
+                
+                -- Убийца убит, выключаем скрипт
+                warn("Убийца убит! Скрипт выключен.")
+                return
+            end
         else
-            warn("У игрока нет персонажа или HumanoidRootPart!")
+            warn("Убийца не найден! Скрипт выключен.")
         end
     else
-        warn("Пистолет не найден на карте!")
+        warn("У вас нет пистолета! Скрипт выключен.")
     end
 end
 
--- Кнопка для подбора оружия (телепортация к пистолету)
-Button(Main, "Grab Gun", function()
-    -- Вызываем функцию телепортации
-    TeleportToGun()
+-- Переключатель для автоматической стрельбы по убийце
+local AutoShootEnabled = false
+
+Toggle(Main, "Auto Shoot Murderer", false, function(state)
+    AutoShootEnabled = state
+    if AutoShootEnabled then
+        AutoShootMurderer()
+    end
 end)
