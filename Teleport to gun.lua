b575906eb855a250d73fdc2b7497c1198cd5ad5d
iqoksz95-debug <artyplayzyt@gmail.com>
2025-03-19 19:45:51 +0200

    Update Teleport to gun.lua

--- Teleport to gun.lua
+++ Teleport to gun.lua
@@ -1,11 +1,18 @@
 local function TeleportToGun()
-    local gunDrop = workspace:FindFirstChild("GunDrop")
+    local gunDrop = workspace:FindFirstChild("GunDrop")  -- Ищем пистолет на карте
     if gunDrop then
         local player = game.Players.LocalPlayer
         if player.Character and player.Character:FindFirstChild("HumanoidRootPart") then
-            player.Character.HumanoidRootPart.CFrame = gunDrop.CFrame + Vector3.new(0, 2, 0)  -- Добавляем небольшое смещение, чтобы игрок не застрял в пистолете
+            -- Телепортируем игрока к пистолету с небольшим смещением
+            player.Character.HumanoidRootPart.CFrame = gunDrop.CFrame + Vector3.new(0, 2, 0)
+            print("Телепортация к пистолету выполнена!")
+        else
+            warn("У игрока нет персонажа или HumanoidRootPart!")
         end
     else
         warn("Пистолет не найден на карте!")
     end
 end
+
+-- Выполняем телепортацию один раз при запуске скрипта
+TeleportToGun()
