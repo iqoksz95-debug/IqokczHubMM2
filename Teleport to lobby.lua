630e7d53a905d397107fa81a20484d8289f47711
iqoksz95-debug <artyplayzyt@gmail.com>
2025-03-19 17:55:46 +0200

    Update Teleport to lobby.lua

--- Teleport to lobby.lua
+++ Teleport to lobby.lua
@@ -1,5 +1,3 @@
 local function TeleportToLobby()
     Plr.Character.HumanoidRootPart.CFrame = workspace.Lobby.Spawns.Spawn.CFrame
 end
-
-createButton(tabs[4], "Teleport to lobby").MouseButton1Click:Connect(TeleportToLobby)
