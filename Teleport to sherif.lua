d58d8aa34b598e4726ed15bd56e9e4617c37249a
iqoksz95-debug <artyplayzyt@gmail.com>
2025-03-19 19:10:27 +0200

    Update Teleport to sherif.lua

--- Teleport to sherif.lua
+++ Teleport to sherif.lua
@@ -1,3 +1,17 @@
+-- Получение локального игрока
+local Plr = game:GetService("Players").LocalPlayer
+
+-- Функция для поиска шерифа
+function GetSheriff()
+    for i, v in pairs(game:GetService("Players"):GetPlayers()) do
+        if (v.Backpack:FindFirstChild("Gun") or (v.Character and v.Character:FindFirstChild("Gun"))) then
+            return v.Character
+        end
+    end
+    return nil
+end
+
+-- Телепорт к шерифу
 local function TeleportToSheriff()
     for i, v in game:GetService("Players"):GetChildren() do
         if v.Backpack:FindFirstChild("Gun") or v.Character:FindFirstChild("Gun") then
