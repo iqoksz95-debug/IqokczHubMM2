e2979ddc448a4003e99459f8463a1f671c2eca5d
iqoksz95-debug <artyplayzyt@gmail.com>
2025-03-19 17:57:28 +0200

    Create Join another server.lua

---
+++ Join another server.lua
@@ -0,0 +1,4 @@
+local function ServerHop()
+    local module = loadstring(game:HttpGet"https://raw.githubusercontent.com/LeoKholYt/roblox/main/lk_serverhop.lua")()
+    module:Teleport(game.PlaceId)
+end
