6971b7e22cce450472606808d134405d75bd5ca7
iqoksz95-debug <artyplayzyt@gmail.com>
2025-03-19 19:17:35 +0200

    Update Teleport to gun.lua

--- Teleport to gun.lua
+++ Teleport to gun.lua
@@ -1,5 +1,28 @@
-local function TeleportToGun()
-    pcall(function()
-        Plr.Character.HumanoidRootPart.CFrame = workspace.GunDrop.CFrame
-    end)
+-- Получение локального игрока
+local Plr = game:GetService("Players").LocalPlayer
+
+-- Функция для поиска сброшенного пистолета
+function FindGunDrop()
+    -- Ищем объект "GunDrop" в рабочем пространстве
+    local gunDrop = workspace:FindFirstChild("GunDrop")
+    if gunDrop then
+        return gunDrop
+    else
+        return nil
+    end
 end
+
+-- Функция для телепорта к сброшенному пистолету
+function TeleportToGunDrop()
+    local gunDrop = FindGunDrop()
+    if gunDrop then
+        -- Телепортируем игрока к позиции пистолета
+        Plr.Character.HumanoidRootPart.CFrame = gunDrop.CFrame
+        print("Телепорт к сброшенному пистолету выполнен!")
+    else
+        warn("Сброшенный пистолет не найден.")
+    end
+end
+
+-- Вызов функции для телепорта
+TeleportToGunDrop()
