3e7b85d91fdb4c33ecbdd2c7b14ce8fde4ccbc25
iqoksz95-debug <artyplayzyt@gmail.com>
2025-03-19 20:09:57 +0200

    Update Kill all.lua

--- Kill all.lua
+++ Kill all.lua
@@ -1,14 +1,39 @@
 -- Функция для убийства всех игроков
 local function KillAll()
-    for i, v in game:GetService("Players"):GetChildren() do
-        if Plr.Backpack:FindFirstChild("Knife") then 
-            Plr.Backpack.Knife.Parent = Plr.Character 
+    -- Получаем локального игрока
+    local Plr = game:GetService("Players").LocalPlayer
+
+    -- Проверяем, есть ли нож у игрока
+    if not Plr.Backpack:FindFirstChild("Knife") and not Plr.Character:FindFirstChild("Knife") then
+        warn("У вас нет ножа!")
+        return
+    end
+
+    -- Перемещаем нож в руку, если он в рюкзаке
+    if Plr.Backpack:FindFirstChild("Knife") then
+        Plr.Backpack.Knife.Parent = Plr.Character
+    end
+
+    -- Проходим по всем игрокам
+    for _, v in pairs(game:GetService("Players"):GetPlayers()) do
+        -- Пропускаем себя
+        if v ~= Plr then
+            -- Проверяем, есть ли персонаж у игрока
+            if v.Character and v.Character:FindFirstChild("HumanoidRootPart") then
+                -- Телепортируемся к игроку
+                Plr.Character.HumanoidRootPart.CFrame = v.Character.HumanoidRootPart.CFrame
+
+                -- Атакуем игрока
+                local args = {
+                    [1] = "Slash"
+                }
+                pcall(function()
+                    Plr.Character.Knife:WaitForChild("Stab"):FireServer(unpack(args))
+                end)
+
+                -- Ждем перед следующей атакой
+                task.wait(0.25)
+            end
         end
-        Plr.Character.HumanoidRootPart.CFrame = v.Character.HumanoidRootPart.CFrame
-        local args = {
-            [1] = "Slash"
-        }
-        Plr.Character.Knife:WaitForChild("Stab"):FireServer(unpack(args))
-        task.wait(.25)
     end
 end
