509c8a5f62c2e80e77200991783e9dfb46b0afb4
iqoksz95-debug <artyplayzyt@gmail.com>
2025-03-19 19:11:52 +0200

    Update Teleport to sherif.lua

--- Teleport to sherif.lua
+++ Teleport to sherif.lua
@@ -12,11 +12,9 @@ function GetSheriff()
 end
 
 -- Телепорт к шерифу
-local function TeleportToSheriff()
-    for i, v in game:GetService("Players"):GetChildren() do
-        if v.Backpack:FindFirstChild("Gun") or v.Character:FindFirstChild("Gun") then
-            Plr.Character.HumanoidRootPart.CFrame = v.Character.HumanoidRootPart.CFrame
-            break
-        end
-    end
+local sheriff = GetSheriff()
+if sheriff then
+    TeleportToPlayer(sheriff)
+else
+    warn("Шериф не найден.")
 end
