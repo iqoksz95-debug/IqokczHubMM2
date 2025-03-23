46ad22a100e4e10df211b5e6e9c179e738200414
iqoksz95-debug <artyplayzyt@gmail.com>
2025-03-23 16:53:05 +0200

    Update Teleport to lobby.lua

--- Teleport to lobby.lua
+++ Teleport to lobby.lua
@@ -1,24 +1,17 @@
--- Получение локального игрока
 local Plr = game:GetService("Players").LocalPlayer
 
--- Функция для поиска точки спавна в лобби
-function FindLobbySpawn()
-    -- Ищем объект спавна в лобби
+local function FindLobbySpawn()
     local lobbySpawn = workspace:FindFirstChild("Lobby")
     if lobbySpawn then
         local spawnPoint = lobbySpawn:FindFirstChild("Spawns")
-        if spawnPoint then
-            return spawnPoint:FindFirstChild("Spawn")
-        end
+        return spawnPoint and spawnPoint:FindFirstChild("Spawn")
     end
     return nil
 end
 
--- Функция для телепорта в лобби
-function TeleportToLobby()
+local function TeleportToLobby()
     local spawnPoint = FindLobbySpawn()
     if spawnPoint then
-        -- Телепортируем игрока к точке спавна в лобби
         Plr.Character.HumanoidRootPart.CFrame = spawnPoint.CFrame
         print("Телепорт в лобби выполнен!")
     else
@@ -26,5 +19,4 @@ function TeleportToLobby()
     end
 end
 
--- Вызов функции для телепорта
 TeleportToLobby()
